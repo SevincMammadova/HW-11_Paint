bc19eec8786bc844fd9ccb24329fa25d7c7487a3
SevincMammadova <sevincmammadova609@gmail.com>
2020-04-17 10:44:11 +0400

    ok

--- src/js/View.js
+++ src/js/View.js
@@ -84,3 +84,63 @@ const createInput = arguments => {
 
 
 module.exports = View;
+
+// const root = document.querySelector('div#root');
+
+// const canvas = document.createElement('canvas');
+// canvas.setAttribute('class', 'canvas');
+// canvas.setAttribute('height', '500px');
+// canvas.setAttribute('width', '500px');
+// root.append(canvas);
+
+// const inputDiv = document.createElement('div');
+// root.appendChild(inputDiv);
+
+// const input = document.createElement('input');
+// input.setAttribute('id', 'color');
+// input.value = '00000';
+// input.type = 'color';
+// inputDiv.appendChild(input); 
+
+// const rangeDiv = document.createElement('div');
+// root.appendChild(rangeDiv);
+
+// const range = document.createElement('input');
+// range.setAttribute('id', 'range');
+// range.type = 'range';
+// rangeDiv.appendChild(range);
+
+// const ctx = canvas.getContext('2d');
+
+// let isPressed = false;
+// let newColor = 'black';
+
+// document.getElementById('color').oninput = function() {
+//     newColor = this.value;
+// }
+// canvas.addEventListener('mousedown', () => {
+//     isPressed = true;
+// })
+
+// canvas.addEventListener('mouseup', () => {
+//     isPressed = false;
+//     ctx.beginPath();
+    
+// })
+
+// ctx.lineWidth = 10 * 2;
+// canvas.addEventListener('mousemove', (e) => {
+//     if(isPressed) {
+//         ctx.lineTo(e.client, e.clientY);
+//         ctx.stroke();
+
+//         ctx.beginPath();
+//         ctx.arc(e.clientX, e.clientY, 10, 0, 2*Math.PI);
+//         ctx.fillStyle = newColor;
+//         ctx.fill();
+
+//         ctx.beginPath();
+//         ctx.moveTo(e.clientX, e.clientY);
+        
+//     }
+// })
