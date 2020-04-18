e050b8e611e9ade5c367a48868cc61a94f67ba2f
SevincMammadova <sevincmammadova609@gmail.com>
2020-04-18 23:30:29 +0400

    ok

--- dest/js/index.js
+++ dest/js/index.js
@@ -1,20 +1,70 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-function Controller(view, model) {
-    this._view = view;
+function Controller(model, view) {
     this._model = model;
+    this._view = view;
+    this._isPressed = false;
+    this._ctx = null;
+    this._color = '';
+    this._width = 0;
+
+    this._x = 0;
+    this._y = 0;
 }
 
 Controller.prototype.init = function() {
-    this._model.init();
-    this._view.init();
+    this._ctx = this._view.getCanvasContext()
+    this._view.onMouseDown(this.startDraw.bind(this));
+    this._view.onMouseUp(this.stopDraw.bind(this));
+    this._view.onMouseMove(this.isMouseDraw.bind(this));
+}
 
-    view.drawing();
+Controller.prototype.startDraw = function(x, y) {
+    this._isPressed = true;
+    
+    this._ctx.strokeStyle = this._color;
+    this._ctx.lineWidth = this._width;
+    this._ctx.lineCap = 'round';
 
-};
+    this._ctx.beginPath();
+    this._ctx.moveTo(x, y);
 
+    this._x = x;
+    this._y = y;
+}
 
+Controller.prototype.stopDraw = function() {
+    this._isPressed = false;
+}
 
+Controller.prototype.isMouseDraw =function(x,y) {
+    if (this._isPressed) {
+        this._ctx.lineTo(x, y);
+        this._ctx.stroke();
+        this._ctx.moveTo(x, y);
 
+        this._addLine(x, y);
+
+        this._x = x;
+        this._y = y;
+    }
+}
+
+Controller.prototype._addLine = function(x, y) {
+    const line = {
+        start: { x: this._x, y: this._y },
+        end: { x, y},
+    }
+
+    return line;
+}
+
+Controller.prototype.changeWidth = function() {
+
+}
+
+Controller.prototype.changeColor = function() {
+
+}
 
 module.exports = Controller;
 },{}],2:[function(require,module,exports){
@@ -31,85 +81,75 @@ module.exports = Model;
 },{}],3:[function(require,module,exports){
 function View() {
     this._root = document.querySelector('div#root');
+    this._canvas = null;
+    this._ctx = null;
+
+    this.init();
+}
+
+const createInput = (id, type, value) => {
+    const wrapper = document.createElement('div');
+
+    const input = document.createElement('input');
+    input.setAttribute('id', id);
+    input.setAttribute('type', type);
+
+    value && (input.value = value);
+    wrapper.append(input);
+
+    return wrapper;
+}
+
+const createCanvas = () => {
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('class', 'canvas');
+    canvas.setAttribute('height', '500px');
+    canvas.setAttribute('width', '1000px');
+
+    this._ctx = canvas.getContext('2d');
+
+    return canvas;
 }
 
 View.prototype.init = function() {
-    const container = createDiv( {
-        class: 'container',
-        id: 'container',
-    });
+    this._canvas = createCanvas();
+    const colorInput = createInput('color', 'color', '#00000');
+    const rangeInput = createInput('range', 'range');
 
-    const container__paint = createDiv({
-        class: 'container__paint',
-    });
+    this._root.append(this._canvas);
+    this._root.append(colorInput);
+    this._root.append(rangeInput);
+}
 
-    const container__input = createDiv({
-        class: 'container__input',
-    });
-    
-    const input__color = createInput({
-        class: 'input__color',
-        type: 'color',
-    });
+View.prototype.getCanvasContext = () => {
+    return this._ctx;
+}
 
-    const input__range = createInput({
-        class: 'input__range',
-        type: 'range',
+View.prototype.onCanvasClick = function(callback) {
+    this._canvas.addEventListener('click', function(event) {
+        const { layerX, layerY } = event;
+        callback(layerX, layerY);
     })
-
-    const canvas = document.createElement('canvas');
-    canvas.setAttribute('class', 'paint__canvas');
-    canvas.setAttribute('id', 'canvas')
-    canvas.setAttribute('height', '500px');
-    canvas.setAttribute('width', '500px');
-
-    container__paint.append(canvas);
-    container__input.append(input__color);
-    container__input.append(input__range);
-    container.append(container__paint);
-    container.append(container__input);
-
-    this._root.append(container);
 }
 
-const createDiv = params => {
-    const div = document.createElement('div');
-    div.setAttribute('class', params.class);
-    params.id && (div.id = params.id);
-    params.title && (div.title = params.title);
-    params.inner && (div.innerHTML = params.inner);
-    params.textContent && (div.textContent = params.textContent);
-
-    return div;
+View.prototype.onMouseDown = function(callback) {
+    this._canvas.addEventListener('mousedown', function(event) {
+        const { layerX, layerY } = event;
+        callback(layerX, layerY);
+    })
 }
 
-const createInput = arguments => {
-    const input = document.createElement('input');
-    input.setAttribute('class', arguments.class);
-    arguments.id && (input.id = arguments.id);
-    arguments.type && (input.type = arguments.type);
-
-    return input;
+View.prototype.onMouseUp = function(callback) {
+    this._canvas.addEventListener('mouseup', function() {
+        callback();
+    })
 }
 
-View.prototype.drawing = function() {
-    const paint = document.getElementById('canvas');
-    const ctx = getContext('2d');
-    let isPressed = false;
-    paint.addEventListener('mousedown', () => {
-    isPressed = true;
-    });
-
-    paint.addEventListener('mouseup', () => {
-    isPressed = false;
-    });
-
-    paint.addEventListener('mousemove', (e) => {
-    ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, 10, 0, 2* Math.PI);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    });
+View.prototype.onMouseMove = function(callback) {
+    this._canvas.addEventListener('mousemove', function(event) {
+        const { layerX, layerY } = event;
+        callback(layerX, layerY);
+    })
 }
 
 module.exports = View;
--- src/js/View.js
+++ src/js/View.js
@@ -1,146 +1,74 @@
 function View() {
     this._root = document.querySelector('div#root');
+    this._canvas = null;
+    this._ctx = null;
+
+    this.init();
+}
+
+const createInput = (id, type, value) => {
+    const wrapper = document.createElement('div');
+
+    const input = document.createElement('input');
+    input.setAttribute('id', id);
+    input.setAttribute('type', type);
+
+    value && (input.value = value);
+    wrapper.append(input);
+
+    return wrapper;
+}
+
+const createCanvas = () => {
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('class', 'canvas');
+    canvas.setAttribute('height', '500px');
+    canvas.setAttribute('width', '1000px');
+
+    this._ctx = canvas.getContext('2d');
+
+    return canvas;
 }
 
 View.prototype.init = function() {
-    const container = createDiv( {
-        class: 'container',
-        id: 'container',
-    });
+    this._canvas = createCanvas();
+    const colorInput = createInput('color', 'color', '#00000');
+    const rangeInput = createInput('range', 'range');
 
-    const container__paint = createDiv({
-        class: 'container__paint',
-    });
+    this._root.append(this._canvas);
+    this._root.append(colorInput);
+    this._root.append(rangeInput);
+}
 
-    const container__input = createDiv({
-        class: 'container__input',
-    });
-    
-    const input__color = createInput({
-        class: 'input__color',
-        type: 'color',
-    });
+View.prototype.getCanvasContext = () => {
+    return this._ctx;
+}
 
-    const input__range = createInput({
-        class: 'input__range',
-        type: 'range',
+View.prototype.onCanvasClick = function(callback) {
+    this._canvas.addEventListener('click', function(event) {
+        const { layerX, layerY } = event;
+        callback(layerX, layerY);
     })
-
-    const canvas = document.createElement('canvas');
-    canvas.setAttribute('class', 'paint__canvas');
-    canvas.setAttribute('id', 'canvas')
-    canvas.setAttribute('height', '500px');
-    canvas.setAttribute('width', '500px');
-
-    container__paint.append(canvas);
-    container__input.append(input__color);
-    container__input.append(input__range);
-    container.append(container__paint);
-    container.append(container__input);
-
-    this._root.append(container);
 }
 
-const createDiv = params => {
-    const div = document.createElement('div');
-    div.setAttribute('class', params.class);
-    params.id && (div.id = params.id);
-    params.title && (div.title = params.title);
-    params.inner && (div.innerHTML = params.inner);
-    params.textContent && (div.textContent = params.textContent);
-
-    return div;
+View.prototype.onMouseDown = function(callback) {
+    this._canvas.addEventListener('mousedown', function(event) {
+        const { layerX, layerY } = event;
+        callback(layerX, layerY);
+    })
 }
 
-const createInput = arguments => {
-    const input = document.createElement('input');
-    input.setAttribute('class', arguments.class);
-    arguments.id && (input.id = arguments.id);
-    arguments.type && (input.type = arguments.type);
-
-    return input;
+View.prototype.onMouseUp = function(callback) {
+    this._canvas.addEventListener('mouseup', function() {
+        callback();
+    })
 }
 
-
-    const paint = document.getElementById('canvas');
-    const ctx = getContext('2d');
-    let isPressed = false;
-    paint.addEventListener('mousedown', () => {
-    isPressed = true;
-    });
-
-    paint.addEventListener('mouseup', () => {
-    isPressed = false;
-    });
-
-    paint.addEventListener('mousemove', (e) => {
-    ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, 10, 0, 2* Math.PI);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    });
-
-    
-
+View.prototype.onMouseMove = function(callback) {
+    this._canvas.addEventListener('mousemove', function(event) {
+        const { layerX, layerY } = event;
+        callback(layerX, layerY);
+    })
+}
 
 module.exports = View;
-
-// const root = document.querySelector('div#root');
-
-// const canvas = document.createElement('canvas');
-// canvas.setAttribute('class', 'canvas');
-// canvas.setAttribute('height', '500px');
-// canvas.setAttribute('width', '500px');
-// root.append(canvas);
-
-// const inputDiv = document.createElement('div');
-// root.appendChild(inputDiv);
-
-// const input = document.createElement('input');
-// input.setAttribute('id', 'color');
-// input.value = '00000';
-// input.type = 'color';
-// inputDiv.appendChild(input); 
-
-// const rangeDiv = document.createElement('div');
-// root.appendChild(rangeDiv);
-
-// const range = document.createElement('input');
-// range.setAttribute('id', 'range');
-// range.type = 'range';
-// rangeDiv.appendChild(range);
-
-// const ctx = canvas.getContext('2d');
-
-// let isPressed = false;
-// let newColor = 'black';
-
-// document.getElementById('color').oninput = function() {
-//     newColor = this.value;
-// }
-// canvas.addEventListener('mousedown', () => {
-//     isPressed = true;
-// })
-
-// canvas.addEventListener('mouseup', () => {
-//     isPressed = false;
-//     ctx.beginPath();
-    
-// })
-
-// ctx.lineWidth = 10 * 2;
-// canvas.addEventListener('mousemove', (e) => {
-//     if(isPressed) {
-//         ctx.lineTo(e.client, e.clientY);
-//         ctx.stroke();
-
-//         ctx.beginPath();
-//         ctx.arc(e.clientX, e.clientY, 10, 0, 2*Math.PI);
-//         ctx.fillStyle = newColor;
-//         ctx.fill();
-
-//         ctx.beginPath();
-//         ctx.moveTo(e.clientX, e.clientY);
-        
-//     }
-// })
